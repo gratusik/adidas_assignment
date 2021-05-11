939dbefe3059b0ff8a4e979ef275f0466a488619
gratusik <agratus@gmail.com>
2021-05-11 14:57:41 +0200

    landscape details xml and acra for crash analytics

--- app/src/main/java/com/gratus/adidasconfirmed/BaseApplication.kt
+++ app/src/main/java/com/gratus/adidasconfirmed/BaseApplication.kt
@@ -6,12 +6,21 @@ import com.gratus.adidasconfirmed.di.component.DaggerAppComponent
 import com.gratus.adidasconfirmed.di.modules.InternetModule
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import org.acra.ACRA
+import org.acra.ReportField
+import org.acra.annotation.AcraCore
+import org.acra.annotation.AcraMailSender
 
+@AcraMailSender(mailTo = "agratus@gmail.com")
+@AcraCore(reportContent = [ReportField.STACK_TRACE, ReportField.LOGCAT])
 open class BaseApplication : DaggerApplication() {
     private lateinit var connectivityManager: ConnectivityManager
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         connectivityManager =
             getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this)
         return DaggerAppComponent.builder().application(this)
             .internetModule(InternetModule(connectivityManager))
             .build()
